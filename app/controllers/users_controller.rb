d8c95b8a410636eb88fdfa9ea140a7a4ada3363d
tungnt-580 <tungnt_580@vnu.edu.vn>
2017-02-28 15:19:08 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 include ActionView::Helpers::TextHelper
 
 class UsersController < ApplicationController
-  before_action :load_user, only: :show
+  before_action :logged_in_user, except: [:new, :create, :show]
+  before_action :load_user, except: [:index, :new, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate page: params[:page]
+  end
 
   def show
   end
@@ -22,7 +29,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".updated"
+      redirect_to @user
+    else
+      flash.now[:danger] = t(".form_contain") + pluralize(@user.errors.count, t(".error"))
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = t ".deleted_notice"
+    redirect_to users_url
+  end
+
   private
+
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
   end
@@ -30,5 +57,21 @@ class UsersController < ApplicationController
   def load_user
     @user = User.find_by id: params[:id]
     raise ActionController::RoutingError.new(t ".not_found") if @user.nil? 
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t ".please_login"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
