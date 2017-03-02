9b91f9b2722336d5963d2a4d16e26b6e4f10c1ad
tungnt-580 <tungnt_580@vnu.edu.vn>
2017-03-02 09:43:58 +0700

    Add password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: t(".activation_subject")
   end
 
-  def password_reset
-    @greeting = t ".hi"
-    mail to: "to@example.org"
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t(".reset_subject")
   end
 end
