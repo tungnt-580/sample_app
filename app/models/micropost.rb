debb009367ca08defbc747b40bf0d0d0627d1d6d
tungnt-580 <tungnt_580@vnu.edu.vn>
2017-03-05 22:17:07 +0700

    --amend

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,15 +1,15 @@
 class Micropost < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, counter_cache: true
   default_scope -> {order created_at: :desc}
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
-  validates :content, presence: true, length: {maximum: 140}
+  validates :content, presence: true, length: {maximum: Settings.validates.content.max_length}
   validate :picture_size
 
   private
 
   def picture_size
-    if picture.size > 5.megabytes
+    if picture.size > Settings.max_size_in_mb.megabytes
       errors.add :picture, t(".wrong_size")
     end
   end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,6 +1,6 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process resize_to_limit: [400, 400]
+  process resize_to_limit: [Settings.limit_size.width, Settings.limit_size.height]
   if Rails.env.production?
     storage :fog
   else
